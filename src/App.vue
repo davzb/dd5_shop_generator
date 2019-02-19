04c379a20ac23ef9955c587001118bd14fc25572
David Bernard <dabernard@SQLI40682-2.local>
2019-02-19 18:05:24 +0100

    Graphic adjustments and i18n yml files

--- src/App.vue
+++ src/App.vue
@@ -22,15 +22,15 @@
                             <label><strong>Size of the shop</strong></label>
                             <div class="control">
                                 <label class="radio">
-                                    <input type="radio" name="shopSize">
+                                    <input type="radio" name="shopSize" data-value="10">
                                     Small
                                 </label>
                                 <label class="radio">
-                                    <input type="radio" name="shopSize" checked>
+                                    <input type="radio" name="shopSize" data-value="20" checked>
                                     Normal
                                 </label>
                                 <label class="radio">
-                                    <input type="radio" name="shopSize">
+                                    <input type="radio" name="shopSize" data-value="30">
                                     Huge
                                 </label>
                             </div>
@@ -71,18 +71,18 @@
                 <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
                     <thead>
                         <tr>
-                            <th>Category</th>
-                            <th>Name</th>
-                            <th>Qty</th>
-                            <th>Lower Price</th>
-                            <th>Medium Price</th>
-                            <th>Higher Price</th>
+                            <th class="is-1 is-size-7 is-uppercase">Category</th>
+                            <th class="is-7 is-size-7 is-uppercase has-text-left">Name</th>
+                            <th class="is-1 is-size-7 is-uppercase">Qty</th>
+                            <th class="is-1 is-size-7 is-uppercase">Lower Price</th>
+                            <th class="is-1 is-size-7 is-uppercase">Medium Price</th>
+                            <th class="is-1 is-size-7 is-uppercase">Higher Price</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>Common</td>
-                            <td>Iron Arrows</td>
+                            <td class="has-text-left">Iron Arrows</td>
                             <td>20</td>
                             <td>8 pc</td>
                             <td>10 pc</td>
@@ -90,8 +90,8 @@
                         </tr>
                         <tr>
                             <td class="has-background-grey-lighter">Rare</td>
-                            <td>
-                                Elixir of Health <span class="has-text-grey">- Potion</span><br>
+                            <td class="has-text-left">
+                                Elixir of Health <span class="has-text-grey is-italic">- Potion</span><br>
                                 <span class="is-size-7">Lorsque vous buvez cette potion, elle guérit de toute maladie qui vous afflige, et supprime les conditions aveuglé, assourdi, paralysé et empoisonné. Le liquide rouge clair possède de minuscules bulles de lumière.</span>
                             </td>
                             <td>1</td>
@@ -101,7 +101,7 @@
                         </tr>
                         <tr>
                             <td class="has-background-warning">Unique</td>
-                            <td></td>
+                            <td class="has-text-left"></td>
                             <td></td>
                             <td></td>
                             <td></td>
@@ -125,4 +125,15 @@ export default {
 </script>
 
 <style lang="scss">
+    .table {
+        tr {
+            td, th { 
+                text-align: center;
+                vertical-align: middle;
+
+                &.is-1 { width: 8.33333%; }
+                &.is-7 { width: 58.33333%; }
+            }
+        }
+    }
 </style>
